a49783e138388764fbddf5d69e5fbc96cde19ec6
SirPacker <git@cheezewerks.com>
2019-09-02 04:03:38 -0400

    Add removeaction command

---
+++ src/commands/removeaction.js
@@ -0,0 +1,23 @@
+const Command = require('../structures/command.js');
+
+module.exports = class extends Command {
+  constructor(client) {
+    super(client, {
+      name: "removeaction",
+      aliases: [],
+      ltu: client.constants.perms.staff
+    });
+  }
+
+  async execute(message) {
+    // removeAction(message, user, aNum)
+    const match = /(?:removeaction)\s+(?:(?:<@!?)?(\d{17,20})>?)(?:\s+(\d+))/.exec(message.content);
+    if (!match) return message.reply("Invalid Syntax: removeaction <user-id/mention> <note #>");
+
+    const user = await this.client.users.fetch(match[1]);
+
+    this.client.handlers.modNotes.removeAction(message, user, match[2])
+      .then(() => message.reply(`Action #${match[2]} for ${user.tag} removed`))
+      .catch(e => message.reply(`ERR: ${e}`));
+  }
+};
