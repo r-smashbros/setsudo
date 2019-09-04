6ca5e32d07b0bd9a0130d400d09ae7a8ce98b154
SirPacker <git@cheezewerks.com>
2019-09-04 17:33:22 -0400

    Allow for multi-line notes

--- src/commands/addnote.js
+++ src/commands/addnote.js
@@ -11,7 +11,7 @@ module.exports = class extends Command {
 
   async execute(message) {
     // addNote(message, user, mod, note)
-    const match = /(?:addnote)\s+(?:(?:<@!?)?(\d{17,20})>?)(?:\s+(.+))/.exec(message.content);
+    const match = /(?:addnote)\s+(?:(?:<@!?)?(\d{17,20})>?)(?:\s+([\w\W]+))/.exec(message.content);
     if (!match) return message.reply("Invalid Syntax: addnote <user-id/mention> <note>");
 
     const user = await this.client.users.fetch(match[1]);
