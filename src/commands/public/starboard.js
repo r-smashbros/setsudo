8ef38f2005dce241780a7664d31fb8834bb711fd
SirPacker <git@cheezewerks.com>
2019-11-27 01:14:19 -0500

    Update starboard command regex

--- src/commands/public/starboard.js
+++ src/commands/public/starboard.js
@@ -12,7 +12,7 @@ module.exports = class extends Command {
 
   async execute(message) {
     // This is black magic fuckery for unicode emoji handling. Touch it and everything breaks
-    const addRegex = /(?:add)(?:\s+(?:<#)?(\d{17,20})(?:>)?)(?:\s+(?:(?:<a?:)(\w+)(?::)(\d+)(?:>)|([\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}])))(?:\s+(\d+))/u
+    const addRegex = /(?:add)(?:\s+(?:<#)?(\d{17,20})(?:>)?)(?:\s+(\\?)(?:(?:<a?:)(\w+)(?::)(\d+)(?:>)|([\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}])))(?:\s+(\d+))/u
       .exec(message.content);
     const removeRegex = /(?:remove)(?:\s+(?:<#)?(\d{17,20})(?:>)?)/.exec(message.content);
     const listRegex = /(?:list)/.exec(message.content);
@@ -36,8 +36,9 @@ module.exports = class extends Command {
 
     if (addRegex) {
       if (!message.guild.channels.get(addRegex[1])) return message.channel.send("ERR: Invalid channel ID given.");
+      if (!addRegex[2]) return message.channel.send("Emoji must be escaped when using this comamnd. Add a `\\` in front of the emoji and try again.");
       
-      return this.client.handlers.starboard.addSB(message, addRegex[1], !!addRegex[4], addRegex[2], addRegex[3], addRegex[5])
+      return this.client.handlers.starboard.addSB(message, addRegex[1], !!addRegex[5], addRegex[3], addRegex[4], addRegex[6])
         .then(() => message.channel.send("Starboard added!"))
         .catch(e => message.channel.send(`ERR: ${e}`));
     } else if (removeRegex) {
