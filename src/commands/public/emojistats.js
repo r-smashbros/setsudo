cfa162d28a9f9dfaad4e745352d5bda68bcef293
Snaacky <snaacky@pm.me>
2019-12-17 10:19:24 -0500

    style: 💄 Fixed missing semis, simplified temp variable names

--- src/commands/public/emojistats.js
+++ src/commands/public/emojistats.js
@@ -12,11 +12,11 @@ module.exports = class extends Command {
   }
 
   async execute(message) {
-    const sorted = new Map([...this.client.db.emojiStats.entries()].sort((usageCount, emojiID) => emojiID[1] - usageCount[1]))
+    const sorted = new Map([...this.client.db.emojiStats.entries()].sort((a, b) => b[1] - a[1]));
     let embedDescription = "";
     sorted.forEach((usageCount, emojiID) => {
-      let emojiName = message.guild.emojis.find(emoji => emoji.id === emojiID).name
-      embedDescription += `<:${emojiName}:${emojiID}> \`${emojiName}: ${usageCount.toLocaleString()} usages\` \n`
+      let emojiName = message.guild.emojis.find(emoji => emoji.id === emojiID).name;
+      embedDescription += `<:${emojiName}:${emojiID}> \`${emojiName}: ${usageCount.toLocaleString()} usages\` \n`;
     });
     const embed = new MessageEmbed().setTitle("Emoji Stats").setDescription(`${embedDescription}`).setColor(0x00FFFF);
     await message.channel.send({ embed: embed });
