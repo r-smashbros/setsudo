1610a83322bd254fdba92d25e94dc03d38bf9fca
Snaacky <snaacky@pm.me>
2019-12-17 11:07:25 -0500

    perf: ⚡️ Replaced dope triple equality check with lame get func

--- src/commands/public/emojistats.js
+++ src/commands/public/emojistats.js
@@ -15,7 +15,7 @@ module.exports = class extends Command {
     const sorted = new Map([...this.client.db.emojiStats.entries()].sort((a, b) => b[1] - a[1]));
     let embedDescription = "";
     sorted.forEach((usageCount, emojiID) => {
-      let emojiName = message.guild.emojis.find(emoji => emoji.id === emojiID).name;
+      let emojiName = message.guild.emojis.get(emojiID).name;
       embedDescription += `<:${emojiName}:${emojiID}> \`${emojiName}: ${usageCount.toLocaleString()} usages\` \n`;
     });
     const embed = new MessageEmbed().setTitle("Emoji Stats").setDescription(`${embedDescription}`).setColor(0x00FFFF);
