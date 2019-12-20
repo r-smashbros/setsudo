a6dd599514c47c76f18821fcb1f75acdff4926c9
Braxton <git@cheezewerks.com>
2019-12-19 21:12:23 -0500

    Ensure that server has emoji before displaying stats

--- src/commands/public/emojistats.js
+++ src/commands/public/emojistats.js
@@ -15,8 +15,8 @@ module.exports = class extends Command {
     const sorted = new Map([...this.client.db.emojiStats.entries()].sort((a, b) => b[1] - a[1]));
     let emojiStats = `__**Emoji Stats for ${message.guild.name}**__\n`;
     sorted.forEach((usageCount, emojiID) => {
-      const emojiName = message.guild.emojis.get(emojiID).name;
-      emojiStats += `<:${emojiName}:${emojiID}> \`${emojiName}: ${usageCount.toLocaleString()} usages\` \n`;
+      const emoji = message.guild.emojis.get(emojiID);
+      if (emoji) emojiStats += `<:${emoji.name}:${emojiID}> \`${emoji.name}: ${usageCount.toLocaleString()} usages\` \n`;
     });
     message.channel.send(emojiStats, { split: true });
   }
