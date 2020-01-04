a1fa1cf8bf39056ca638a74d0352276771c1addb
Braxton <mahrb@icloud.com>
2020-01-04 04:35:56 -0500

    docs: ✏️ Add JSDoc for addnote command

--- src/commands/public/addnote.js
+++ src/commands/public/addnote.js
@@ -10,6 +10,11 @@ module.exports = class extends Command {
     });
   }
 
+  /**
+   * Entry point for addnote command
+   * @param {Message} message The message that invoked the command
+   * @returns {Message|null} Returns Message instance if an error occurs. Otherwise, nothing is returned.
+   */
   async execute(message) {
     // addNote(message, user, mod, note)
     const match = /(?:addnote)\s+(?:(?:<@!?)?(\d{17,20})>?)(?:\s+([\w\W]+))/.exec(message.content);
