583a284d9fc356e2de8fd9dea58be957998d4208
Braxton <mahrb@icloud.com>
2020-01-04 03:54:04 -0500

    docs: ✏️ Add JSDoc for ping command

--- src/commands/public/ping.js
+++ src/commands/public/ping.js
@@ -10,6 +10,10 @@ module.exports = class extends Command {
     });
   }
 
+  /**
+   * Entry point for ping command
+   * @param {Message} message The message that invoked the command
+   */
   async execute(message) {
     message.channel.send("Pinging...")
       .then(m => m.edit(`Client Ping: ${m.createdTimestamp - message.createdTimestamp} | API Ping: ${this.client.ws.ping}`));
