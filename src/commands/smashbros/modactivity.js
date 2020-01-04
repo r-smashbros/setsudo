0b7df0a310b2aa70746cf91209bacfc086274bf9
Braxton <mahrb@icloud.com>
2020-01-04 03:05:13 -0500

    docs: ✏️ Add JSDoc for modactivity command

--- src/commands/smashbros/modactivity.js
+++ src/commands/smashbros/modactivity.js
@@ -11,7 +11,12 @@ module.exports = class extends Command {
     });
   }
 
+  /**
+   * Entry point for modactivity command
+   * @param {Message} message The message that invoked the command
+   */
   async execute(message) {
+    // Check to see if the stats need to be cleared after execution
     const clearStats = /--clear/.test(message.content);
 
     let aStr = "";
@@ -20,20 +25,22 @@ module.exports = class extends Command {
     const mObj = {}, mArr = [];
     let vStr = "";
 
+    // Loop over each entry in the activityStats DB
     this.client.db.activityStats.forEach((v, k) => {
+      // Separate actions and messages for sorting
       aArr.push([k, v["actions"]]);
       mArr.push([k, v["messages"]]);
-
-      //const user = await this.client.users.fetch(k);
-      //aStr += `${user.tag}: ${v["actions"]}\n`;
-      //mStr += `${user.tag}: ${v["messages"]}\n`;
     });
 
     // Sort action and message counts
     aArr.sort((a, b) => b[1] - a[1]);
     mArr.sort((a, b) => b[1] - a[1]);
+
+    // Restore action and message counts to objects from arrays
     for (const entry of aArr) aObj[entry[0]] = entry[1];
     for (const entry of mArr) mObj[entry[0]] = entry[1];
+
+    // Loop over action and message stats and append to strings
     for (const [k, v] of Object.entries(aObj)) {
       const user = await this.client.users.fetch(k);
       aStr += `${user.tag}:: ${v}\n`;
@@ -43,13 +50,16 @@ module.exports = class extends Command {
       mStr += `${user.tag}:: ${v}\n`;
     }
 
-    const vStats = await this.getVoteUsers();
+    // Fetch voting statistics
+    const vStats = await this._getVoteUsers();
 
+    // Loop over vote stats and append to string
     for (const [k, v] of Object.entries(vStats)) {
       const user = await this.client.users.fetch(k);
       vStr += `${user.tag}:: ${v}\n`;
     }
 
+    // Construct and send action, message, and missed votes embeds
     const aEmbed = new MessageEmbed().setTitle("Action Activity").setDescription(`\`\`\`asciidoc\n${aStr}\`\`\``).setColor(0xFF0000);
     const mEmbed = new MessageEmbed().setTitle("Message Activity").setDescription(`\`\`\`asciidoc\n${mStr}\`\`\``).setColor(0x00FF00);
     const vEmbed = new MessageEmbed().setTitle("Missed Votes").setDescription(`\`\`\`asciidoc\n${vStr}\`\`\``).setColor(0x0000FF).setTimestamp();
@@ -58,22 +68,32 @@ module.exports = class extends Command {
     await message.channel.send({ embed: mEmbed });
     await message.channel.send({ embed: vEmbed });
 
+    // Clear stats if the "--clear" flag was present
     if (clearStats) this.client.db.activityStats.deleteAll();
   }
 
-  getVoteUsers() {
+  /**
+   * Fetch all votes within a two week time frame and return number of missed votes
+   * @private
+   */
+  _getVoteUsers() {
     // eslint-disable-next-line no-async-promise-executor
     return new Promise(async (resolve, reject) => {
       let rUsers = {};
 
+      // Fetch votes and filter out messages older than two weeks and without reactions
       const voteChan = this.client.guilds.get(this.client.config["servSpec"]["modServ"]).channels.get(this.client.config["servSpec"]["voteChan"]);
       let voteMsg = await voteChan.messages.fetch({ limit: 100 });
       voteMsg = voteMsg.filter(m => m.createdTimestamp > Date.now() - 1209600000 && m.reactions.size);
 
+      // Loop over each vote
       for (const msg of voteMsg.values()) {
+        // Loop over each reaction on a vote
         for (const r of msg.reactions.values()) {
+          // Fetch the users who reacted
           const _rUsers = await r.users.fetch();
 
+          // Loop over each user who reacted
           for (const u of _rUsers.values()) {
             if (u.id === this.client.user.id) continue;
             if (!rUsers[u.id]) rUsers[u.id] = 0;
@@ -82,6 +102,7 @@ module.exports = class extends Command {
         }
       }
 
+      // Subtract number of reactions from number of messages
       for (const val of Object.keys(rUsers)) {
         if (rUsers[val] >= voteMsg.size) { rUsers[val] = 0; continue; }
         rUsers[val] = voteMsg.size - rUsers[val];
