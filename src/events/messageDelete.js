bf7dfa04d7231b5be6a19e45690e4674ca01c605
Braxton <mahrb@icloud.com>
2020-01-08 07:38:48 -0500

    feat: 🎸 Add messageDelete event and logging

---
+++ src/events/messageDelete.js
@@ -0,0 +1,33 @@
+const Event = require('../structures/event.js');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class extends Event {
+  constructor(client) {
+    super(client, {
+      name: "messageDelete"
+    });
+  }
+
+  /**
+   * Entry point for messageDelete event
+   * @param {Message} message The deleted message
+   */
+  execute(message) {
+    if (!message.content) return;
+
+    const guild = message.guild;
+    const gSettings = this.client.db.settings.get(guild.id);
+
+    if (gSettings["messagelogschannel"] && guild.channels.get(gSettings["messagelogschannel"])) {
+      const messageLogsChan = guild.channels.get(gSettings["messagelogschannel"]);
+      messageLogsChan.send({
+        embed: new MessageEmbed()
+          .setAuthor(`${message.author.tag} (${message.author.id})`, message.author.avatarURL())
+          .setColor(this.client.constants.colours.delete)
+          .setDescription(message.content)
+          .setFooter("Message Deleted")
+          .setTimestamp()
+      });
+    }
+  }
+};
