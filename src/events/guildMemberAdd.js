fe31e17306b7e75823385fbe642e9df7d9472386
Braxton <mahrb@icloud.com>
2020-01-08 07:23:21 -0500

    feat: 🎸 Add guildMemberAdd event and logging

---
+++ src/events/guildMemberAdd.js
@@ -0,0 +1,31 @@
+const Event = require("../structures/event.js");
+const moment = require("moment");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = class extends Event {
+  constructor(client) {
+    super(client, {
+      name: "guildMemberAdd"
+    });
+  }
+
+  /**
+   * Entry point for guildMemberAdd event
+   * @param {GuildMember} ctx The member that joined the server
+   */
+  async execute(ctx = null) {
+    const guild = ctx.guild;
+    const gSettings = this.client.db.settings.get(guild.id);
+
+    if (gSettings["memberlogschannel"] && guild.channels.get(gSettings["memberlogschannel"])) {
+      const memberLogsChan = guild.channels.get(gSettings["memberlogschannel"]);
+      memberLogsChan.send({ 
+        embed: new MessageEmbed()
+          .setAuthor(`${ctx.user.tag} (${ctx.user.id})`, ctx.user.avatarURL())
+          .setColor(this.client.constants.colours.join)
+          .setDescription("User Joined")
+          .setTimestamp()
+      });
+    }
+  }
+};
